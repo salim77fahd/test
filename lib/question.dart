6bf9c86a13e3672beea15513468b5ce1c42c8125
Salim Fahd <salim777fahd@gmail.com>
2025-08-02 22:31:35 +0300

    second changes

---
+++ lib/question.dart
@@ -0,0 +1,8 @@
+class Question {
+  String questionText = '';
+  bool questionAnswer = false;
+  Question({required String q, required bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
